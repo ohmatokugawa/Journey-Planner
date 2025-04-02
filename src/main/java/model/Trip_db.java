9b1806b014fcc76c66c01abd26eca2308b843ef5
Dominik Haładus <dominik.haladus@protonmail.com>
2025-04-02 12:40:37 +0200

    Trips_db config

---
+++ src/main/java/model/Trip_db.java
@@ -0,0 +1,23 @@
+package model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class Trip_db {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String destination;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String transport;
+}
